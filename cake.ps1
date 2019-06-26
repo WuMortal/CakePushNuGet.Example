45cad3e3865575247cdc68aef1a4cd926a1694cc
wumeng <1170621609@qq.com>
2019-06-26 12:38:46 +0800

    [update] cake.ps1 update

--- cake.ps1
+++ cake.ps1
@@ -1,7 +1,21 @@
+# 执行的文件
+[string]$SCRIPT = 'build/build.cake'
+
+[string]$CAKE_VERSION = '0.33.0'
+
+# 配置 NuGet 环境变量
+$NUGET_EXE = "build/tool/NuGet.exe"
+$NUGET_DIRECTORY = Get-ChildItem -Path $NUGET_EXE
+$NUGET_DIRECTORY_NAME=$NUGET_DIRECTORY.DirectoryName
+$env:Path += ";$NUGET_DIRECTORY_NAME"
+
 # Install cake.tool
-dotnet tool install --global cake.tool --version 0.33.0
+dotnet tool install --global cake.tool --version $CAKE_VERSION
+
+# 参数：显需要执行cake 执行信息
+[string]$CAKE_ARGS = "-verbosity=diagnostic"
 
 # 输出将要执行的命命令
 Write-Host "dotnet cake $SCRIPT $CAKE_ARGS $ARGS" -ForegroundColor GREEN
 
-dotnet cake build\build.cake -verbosity=diagnostic
+dotnet cake $SCRIPT $CAKE_ARGS $ARGS
